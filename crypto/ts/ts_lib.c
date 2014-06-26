e8602536dd6bace4cd74d08257618aa259908146
Kurt Cancemi <kurt@x64Architecture.com>
2014-06-26 02:30:40 -0400

    Don't write out more than we have allocated in obj_txt, as the glory
    that is OBJ_obj2txt() can return a larger value..
    
    Patch by tedu (OpeBSD)

--- crypto/ts/ts_lib.c
+++ crypto/ts/ts_lib.c
@@ -90,6 +90,8 @@ int TS_OBJ_print_bio(BIO *bio, const ASN1_OBJECT *obj)
     char obj_txt[128];
 
     int len = OBJ_obj2txt(obj_txt, sizeof(obj_txt), obj, 0);
+    if (len >= sizeof(obj_txt))
+        len = sizeof(obj_txt) - 1;
     BIO_write(bio, obj_txt, len);
     BIO_write(bio, "\n", 1);
 
