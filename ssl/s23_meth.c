d669bd99bc3e54749d269524afcfa3014a432c0e
Kurt Cancemi <kurt@x64Architecture.com>
2014-07-01 01:39:49 -0400

    Style.

--- ssl/s23_meth.c
+++ ssl/s23_meth.c
@@ -62,26 +62,26 @@
 
 static const SSL_METHOD *ssl23_get_method(int ver);
 static const SSL_METHOD *ssl23_get_method(int ver)
-	{
+{
 #ifndef OPENSSL_NO_SSL3
-	if (ver == SSL3_VERSION)
-		return(SSLv3_method());
-	else
+    if (ver == SSL3_VERSION)
+        return (SSLv3_method());
+    else
 #endif
 #ifndef OPENSSL_NO_TLS1
-	if (ver == TLS1_VERSION)
-		return(TLSv1_method());
-	else if (ver == TLS1_1_VERSION)
-		return(TLSv1_1_method());
-	else if (ver == TLS1_2_VERSION)
-		return(TLSv1_2_method());
-	else
+    if (ver == TLS1_VERSION)
+        return (TLSv1_method());
+    else if (ver == TLS1_1_VERSION)
+        return (TLSv1_1_method());
+    else if (ver == TLS1_2_VERSION)
+        return (TLSv1_2_method());
+    else
 #endif
-		return(NULL);
-	}
+        return (NULL);
+}
 
 IMPLEMENT_ssl23_meth_func(SSLv23_method,
-			ssl23_accept,
-			ssl23_connect,
-			ssl23_get_method)
+            ssl23_accept,
+            ssl23_connect,
+            ssl23_get_method)
 
