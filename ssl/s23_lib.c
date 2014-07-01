8a2e8e9b484bfb3600d3838f3c2d4feb86e9e9e0
Kurt Cancemi <kurt@x64Architecture.com>
2014-07-01 01:45:17 -0400

    Style.

--- ssl/s23_lib.c
+++ ssl/s23_lib.c
@@ -61,115 +61,105 @@
 #include "ssl_locl.h"
 
 long ssl23_default_timeout(void)
-	{
-	return(300);
-	}
+{
+    return (300);
+}
 
 int ssl23_num_ciphers(void)
-	{
-	return(ssl3_num_ciphers());
-	}
+{
+    return (ssl3_num_ciphers());
+}
 
 const SSL_CIPHER *ssl23_get_cipher(unsigned int u)
-	{
-	unsigned int uu=ssl3_num_ciphers();
+{
+    unsigned int uu=ssl3_num_ciphers();
 
-	if (u < uu)
-		return(ssl3_get_cipher(u));
-	else
-		return (NULL);
-	}
+    if (u < uu)
+        return(ssl3_get_cipher(u));
+    else
+        return (NULL);
+}
 
 /* This function needs to check if the ciphers required are actually
  * available */
 const SSL_CIPHER *ssl23_get_cipher_by_char(const unsigned char *p)
-	{
-	const SSL_CIPHER *cp;
+{
+    const SSL_CIPHER *cp;
 
-	cp=ssl3_get_cipher_by_char(p);
-	return(cp);
-	}
+    cp = ssl3_get_cipher_by_char(p);
+    return (cp);
+}
 
 int ssl23_put_cipher_by_char(const SSL_CIPHER *c, unsigned char *p)
-	{
-	long l;
+{
+    long l;
 
-	/* We can write SSLv2 and SSLv3 ciphers */
-	if (p != NULL)
-		{
-		l=c->id;
-		p[0]=((unsigned char)(l>>16L))&0xFF;
-		p[1]=((unsigned char)(l>> 8L))&0xFF;
-		p[2]=((unsigned char)(l     ))&0xFF;
-		}
-	return(3);
-	}
+    /* We can write SSLv2 and SSLv3 ciphers */
+    if (p != NULL) {
+        l=c->id;
+        p[0] = ((unsigned char)(l >>16L)) & 0xFF;
+        p[1] = ((unsigned char)(l >> 8L)) & 0xFF;
+        p[2] = ((unsigned char)(l)) & 0xFF;
+    }
+    return (3);
+}
 
 int ssl23_read(SSL *s, void *buf, int len)
-	{
-	int n;
+{
+    int n;
 
-	clear_sys_error();
-	if (SSL_in_init(s) && (!s->in_handshake))
-		{
-		n=s->handshake_func(s);
-		if (n < 0) return(n);
-		if (n == 0)
-			{
-			SSLerr(SSL_F_SSL23_READ,SSL_R_SSL_HANDSHAKE_FAILURE);
-			return(-1);
-			}
-		return(SSL_read(s,buf,len));
-		}
-	else
-		{
-		ssl_undefined_function(s);
-		return(-1);
-		}
-	}
+    clear_sys_error();
+    if (SSL_in_init(s) && (!s->in_handshake)) {
+        n = s->handshake_func(s);
+        if (n < 0)
+            return (n);
+        if (n == 0) {
+            SSLerr(SSL_F_SSL23_READ, SSL_R_SSL_HANDSHAKE_FAILURE);
+            return(-1);
+        }
+        return (SSL_read(s, buf, len));
+    } else {
+        ssl_undefined_function(s);
+        return (-1);
+    }
+}
 
 int ssl23_peek(SSL *s, void *buf, int len)
-	{
-	int n;
+{
+    int n;
 
-	clear_sys_error();
-	if (SSL_in_init(s) && (!s->in_handshake))
-		{
-		n=s->handshake_func(s);
-		if (n < 0) return(n);
-		if (n == 0)
-			{
-			SSLerr(SSL_F_SSL23_PEEK,SSL_R_SSL_HANDSHAKE_FAILURE);
-			return(-1);
-			}
-		return(SSL_peek(s,buf,len));
-		}
-	else
-		{
-		ssl_undefined_function(s);
-		return(-1);
-		}
-	}
+    clear_sys_error();
+    if (SSL_in_init(s) && (!s->in_handshake)) {
+        n = s->handshake_func(s);
+        if (n < 0)
+            return (n);
+        if (n == 0) {
+            SSLerr(SSL_F_SSL23_PEEK, SSL_R_SSL_HANDSHAKE_FAILURE);
+            return (-1);
+        }
+        return (SSL_peek(s, buf, len));
+    } else {
+        ssl_undefined_function(s);
+        return (-1);
+    }
+}
 
 int ssl23_write(SSL *s, const void *buf, int len)
-	{
-	int n;
+{
+    int n;
 
-	clear_sys_error();
-	if (SSL_in_init(s) && (!s->in_handshake))
-		{
-		n=s->handshake_func(s);
-		if (n < 0) return(n);
-		if (n == 0)
-			{
-			SSLerr(SSL_F_SSL23_WRITE,SSL_R_SSL_HANDSHAKE_FAILURE);
-			return(-1);
-			}
-		return(SSL_write(s,buf,len));
-		}
-	else
-		{
-		ssl_undefined_function(s);
-		return(-1);
-		}
-	}
+    clear_sys_error();
+    if (SSL_in_init(s) && (!s->in_handshake)) {
+        n = s->handshake_func(s);
+        if (n < 0)
+            return (n);
+        if (n == 0) {
+            SSLerr(SSL_F_SSL23_WRITE, SSL_R_SSL_HANDSHAKE_FAILURE);
+            return (-1);
+        }
+        return (SSL_write(s, buf, len));
+    } else {
+        ssl_undefined_function(s);
+        return (-1);
+    }
+}
