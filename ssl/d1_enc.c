266601998b5f4b707bf0959d6bc72f5eaad9f5b6
Kurt Cancemi <kurt@x64Architecture.com>
2014-07-04 00:32:07 -0400

    Style.

--- ssl/d1_enc.c
+++ ssl/d1_enc.c
@@ -132,95 +132,81 @@
  *   -1: if the record's padding/AEAD-authenticator is invalid or, if sending,
  *       an internal error occured. */
 int dtls1_enc(SSL *s, int send)
-	{
-	SSL3_RECORD *rec;
-	EVP_CIPHER_CTX *ds;
-	unsigned long l;
-	int bs,i,j,k,mac_size=0;
-	const EVP_CIPHER *enc;
+{
+    SSL3_RECORD *rec;
+    EVP_CIPHER_CTX *ds;
+    unsigned long l;
+    int bs, i, j, k, mac_size = 0;
+    const EVP_CIPHER *enc;
 
-	if (send)
-		{
-		if (EVP_MD_CTX_md(s->write_hash))
-			{
-			mac_size=EVP_MD_CTX_size(s->write_hash);
-			if (mac_size < 0)
-				return -1;
-			}
-		ds=s->enc_write_ctx;
-		rec= &(s->s3->wrec);
-		if (s->enc_write_ctx == NULL)
-			enc=NULL;
-		else
-			{
-			enc=EVP_CIPHER_CTX_cipher(s->enc_write_ctx);
-			if ( rec->data != rec->input)
-				/* we can't write into the input stream */
-				fprintf(stderr, "%s:%d: rec->data != rec->input\n",
-					__FILE__, __LINE__);
-			else if ( EVP_CIPHER_block_size(ds->cipher) > 1)
-				{
-				if (RAND_bytes(rec->input, EVP_CIPHER_block_size(ds->cipher)) <= 0)
-					return -1;
-				}
-			}
-		}
-	else
-		{
-		if (EVP_MD_CTX_md(s->read_hash))
-			{
-			mac_size=EVP_MD_CTX_size(s->read_hash);
-			OPENSSL_assert(mac_size >= 0);
-			}
-		ds=s->enc_read_ctx;
-		rec= &(s->s3->rrec);
-		if (s->enc_read_ctx == NULL)
-			enc=NULL;
-		else
-			enc=EVP_CIPHER_CTX_cipher(s->enc_read_ctx);
-		}
+    if (send) {
+        if (EVP_MD_CTX_md(s->write_hash)) {
+            mac_size = EVP_MD_CTX_size(s->write_hash);
+            if (mac_size < 0)
+                return -1;
+        }
+        ds = s->enc_write_ctx;
+        rec = &(s->s3->wrec);
+        if (s->enc_write_ctx == NULL)
+            enc = NULL;
+        else {
+            enc = EVP_CIPHER_CTX_cipher(s->enc_write_ctx);
+            if (rec->data != rec->input)
+                /* we can't write into the input stream */
+                fprintf(stderr, "%s:%d: rec->data != rec->input\n",
+                    __FILE__, __LINE__);
+            else if (EVP_CIPHER_block_size(ds->cipher) > 1) {
+                if (RAND_bytes(rec->input, EVP_CIPHER_block_size(ds->cipher)) <= 0)
+                    return -1;
+                }
+            }
+    } else {
+        if (EVP_MD_CTX_md(s->read_hash)) {
+            mac_size = EVP_MD_CTX_size(s->read_hash);
+            OPENSSL_assert(mac_size >= 0);
+        }
+        ds = s->enc_read_ctx;
+        rec = &(s->s3->rrec);
+        if (s->enc_read_ctx == NULL)
+            enc = NULL;
+        else
+            enc = EVP_CIPHER_CTX_cipher(s->enc_read_ctx);
+    }
 
-	if ((s->session == NULL) || (ds == NULL) ||
-		(enc == NULL))
-		{
-		memmove(rec->data,rec->input,rec->length);
-		rec->input=rec->data;
-		}
-	else
-		{
-		l=rec->length;
-		bs=EVP_CIPHER_block_size(ds->cipher);
+    if ((s->session == NULL) || (ds == NULL) || (enc == NULL)) {
+        memmove(rec->data, rec->input, rec->length);
+        rec->input = rec->data;
+    } else {
+        l = rec->length;
+        bs = EVP_CIPHER_block_size(ds->cipher);
 
-		if ((bs != 1) && send)
-			{
-			i=bs-((int)l%bs);
+        if ((bs != 1) && send) {
+            i = bs- ((int)l % bs);
 
-			/* Add weird padding of upto 256 bytes */
+            /* Add weird padding of upto 256 bytes */
 
-			/* we need to add 'i' padding bytes of value j */
-			j=i-1;
-			if (s->options & SSL_OP_TLS_BLOCK_PADDING_BUG)
-				{
-				if (s->s3->flags & TLS1_FLAGS_TLS_PADDING_BUG)
-					j++;
-				}
-			for (k=(int)l; k<(int)(l+i); k++)
-				rec->input[k]=j;
-			l+=i;
-			rec->length+=i;
-			}
+            /* we need to add 'i' padding bytes of value j */
+            j = i - 1;
+            if (s->options & SSL_OP_TLS_BLOCK_PADDING_BUG) {
+                if (s->s3->flags & TLS1_FLAGS_TLS_PADDING_BUG)
+                    j++;
+            }
+            for (k = (int)l; k < (int)(l + i); k++)
+                rec->input[k] = j;
+            l += i;
+            rec->length += i;
+        }
 
-		if (!send)
-			{
-			if (l == 0 || l%bs != 0)
-				return 0;
-			}
-		
-		EVP_Cipher(ds,rec->data,rec->input,l);
+        if (!send) {
+            if (l == 0 || l%bs != 0)
+                return 0;
+        }
+        
+        EVP_Cipher(ds, rec->data, rec->input, l);
 
-		if ((bs != 1) && !send)
-			return tls1_cbc_remove_padding(s, rec, bs, mac_size);
-		}
-	return(1);
-	}
+        if ((bs != 1) && !send)
+            return tls1_cbc_remove_padding(s, rec, bs, mac_size);
+    }
+    return (1);
+}
 
