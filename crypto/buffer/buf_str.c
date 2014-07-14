f753cfd834fc439b9119a62c5a15b24977d3eaca
Kurt Cancemi <kurt@x64Architecture.com>
2014-07-14 08:38:12 -0400

    Use standard functions instead of implementing them.

--- crypto/buffer/buf_str.c
+++ crypto/buffer/buf_str.c
@@ -100,20 +100,10 @@ void *BUF_memdup(const void *data, size_t siz)
 
 size_t BUF_strlcpy(char *dst, const char *src, size_t size)
 {
-    size_t l = 0;
-    for(; size > 1 && *src; size--) {
-        *dst++ = *src++;
-        l++;
-    }
-    if (size)
-        *dst = '\0';
-    return l + strlen(src);
+    return strlcpy(dst, src, size);
 }
 
 size_t BUF_strlcat(char *dst, const char *src, size_t size)
 {
-    size_t l = 0;
-    for(; size > 0 && *dst; size--, dst++)
-        l++;
-    return l + BUF_strlcpy(dst, src, size);
+    return strlcat(dst, src, size)
 }
