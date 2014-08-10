f828d0d57d2c0248ae1182ed108e6578e720ab86
Kurt Cancemi <kurt@x64Architecture.com>
2014-08-10 08:13:48 -0400

    Style.

--- ssl/ssl_cert.c
+++ ssl/ssl_cert.c
@@ -5,21 +5,21 @@
  * This package is an SSL implementation written
  * by Eric Young (eay@cryptsoft.com).
  * The implementation was written so as to conform with Netscapes SSL.
- * 
+ *
  * This library is free for commercial and non-commercial use as long as
  * the following conditions are aheared to.  The following conditions
  * apply to all code found in this distribution, be it the RC4, RSA,
  * lhash, DES, etc., code; not just the SSL code.  The SSL documentation
  * included with this distribution is covered by the same copyright terms
  * except that the holder is Tim Hudson (tjh@cryptsoft.com).
- * 
+ *
  * Copyright remains Eric Young's, and as such any Copyright notices in
  * the code are not to be removed.
  * If this package is used in a product, Eric Young should be given attribution
  * as the author of the parts of the library used.
  * This can be in the form of a textual message at program startup or
  * in documentation (online or textual) provided with the package.
- * 
+ *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions
  * are met:
@@ -34,10 +34,10 @@
  *     Eric Young (eay@cryptsoft.com)"
  *    The word 'cryptographic' can be left out if the rouines from the library
  *    being used are not cryptographic related :-).
- * 4. If you include any Windows specific code (or a derivative thereof) from 
+ * 4. If you include any Windows specific code (or a derivative thereof) from
  *    the apps directory (application code) you must include an acknowledgement:
  *    "This product includes software written by Tim Hudson (tjh@cryptsoft.com)"
- * 
+ *
  * THIS SOFTWARE IS PROVIDED BY ERIC YOUNG ``AS IS'' AND
  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
@@ -49,7 +49,7 @@
  * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY
  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
  * SUCH DAMAGE.
- * 
+ *
  * The licence and distribution terms for any publically available version or
  * derivative of this code cannot be changed.  i.e. this code cannot simply be
  * copied and put under another distribution licence
@@ -63,7 +63,7 @@
  * are met:
  *
  * 1. Redistributions of source code must retain the above copyright
- *    notice, this list of conditions and the following disclaimer. 
+ *    notice, this list of conditions and the following disclaimer.
  *
  * 2. Redistributions in binary form must reproduce the above copyright
  *    notice, this list of conditions and the following disclaimer in
@@ -110,7 +110,7 @@
  */
 /* ====================================================================
  * Copyright 2002 Sun Microsystems, Inc. ALL RIGHTS RESERVED.
- * ECC cipher suite support in OpenSSL originally developed by 
+ * ECC cipher suite support in OpenSSL originally developed by
  * SUN MICROSYSTEMS, INC., and contributed to the OpenSSL project.
  */
 
@@ -137,7 +137,7 @@ int SSL_get_ex_data_X509_STORE_CTX_idx(void)
         CRYPTO_r_unlock(CRYPTO_LOCK_SSL_CTX);
         CRYPTO_w_lock(CRYPTO_LOCK_SSL_CTX);
         got_write_lock = 1;
-        
+
         if (ssl_x509_store_ctx_idx < 0) {
             ssl_x509_store_ctx_idx = X509_STORE_CTX_get_ex_new_index(
                 0, "SSL for verify callback", NULL, NULL, NULL);
@@ -148,7 +148,7 @@ int SSL_get_ex_data_X509_STORE_CTX_idx(void)
         CRYPTO_w_unlock(CRYPTO_LOCK_SSL_CTX);
     else
         CRYPTO_r_unlock(CRYPTO_LOCK_SSL_CTX);
-    
+
     return ssl_x509_store_ctx_idx;
 }
 
@@ -250,7 +250,7 @@ CERT *ssl_cert_dup(CERT *cert)
             CRYPTO_add(&ret->pkeys[i].x509->references, 1,
                 CRYPTO_LOCK_X509);
         }
-        
+
         if (cert->pkeys[i].privatekey != NULL) {
             ret->pkeys[i].privatekey = cert->pkeys[i].privatekey;
             CRYPTO_add(&ret->pkeys[i].privatekey->references, 1,
@@ -265,11 +265,11 @@ CERT *ssl_cert_dup(CERT *cert)
             case SSL_PKEY_RSA_SIGN:
                 /* We have an RSA key. */
                 break;
-                
+
             case SSL_PKEY_DSA_SIGN:
                 /* We have a DSA key. */
                 break;
-                
+
             case SSL_PKEY_DH_RSA:
             case SSL_PKEY_DH_DSA:
                 /* We have a DH key. */
@@ -285,7 +285,7 @@ CERT *ssl_cert_dup(CERT *cert)
             }
         }
     }
-    
+
     /* ret->extra_certs *should* exist, but currently the own certificate
      * chain is held inside SSL_CTX */
 
@@ -296,7 +296,7 @@ CERT *ssl_cert_dup(CERT *cert)
     ssl_cert_set_default_md(ret);
 
     return (ret);
-    
+
 #if !defined(OPENSSL_NO_ECDH)
 err:
 #endif
@@ -373,7 +373,7 @@ int ssl_cert_inst(CERT **o)
      * s->cert being NULL, otherwise we could do without the
      * initialization in SSL_CTX_new).
      */
-    
+
     if (o == NULL) {
         SSLerr(SSL_F_SSL_CERT_INST, ERR_R_PASSED_NULL_PARAMETER);
         return (0);
@@ -491,7 +491,7 @@ int ssl_verify_cert_chain(SSL *s, STACK_OF(X509) *sk)
 
     if (s->ctx->app_verify_callback != NULL)
 #if 1 /* new with OpenSSL 0.9.7 */
-        i = s->ctx->app_verify_callback(&ctx, s->ctx->app_verify_arg); 
+        i = s->ctx->app_verify_callback(&ctx, s->ctx->app_verify_arg);
 #else
         i = s->ctx->app_verify_callback(&ctx); /* should pass app_verify_arg */
 #endif
@@ -575,7 +575,7 @@ static int add_client_CA(STACK_OF(X509_NAME) **sk, X509 *x)
         return (0);
     if ((*sk == NULL) && ((*sk = sk_X509_NAME_new_null()) == NULL))
         return (0);
-        
+
     if ((name = X509_NAME_dup(X509_get_subject_name(x))) == NULL)
         return (0);
 
@@ -610,7 +610,7 @@ static int xname_cmp(const X509_NAME * const *a, const X509_NAME * const *b)
  * \return a ::STACK containing the certs.
  */
 STACK_OF(X509_NAME) *SSL_load_client_CA_file(const char *file)
-    {
+{
     BIO *in;
     X509 *x = NULL;
     X509_NAME *xn = NULL;
@@ -618,13 +618,13 @@ STACK_OF(X509_NAME) *SSL_load_client_CA_file(const char *file)
 
     sk = sk_X509_NAME_new(xname_cmp);
 
-    in=BIO_new(BIO_s_file_internal());
+    in = BIO_new(BIO_s_file_internal());
 
     if ((sk == NULL) || (in == NULL)) {
         SSLerr(SSL_F_SSL_LOAD_CLIENT_CA_FILE, ERR_R_MALLOC_FAILURE);
         goto err;
     }
-    
+
     if (!BIO_read_filename(in, file))
         goto err;
 
@@ -686,19 +686,19 @@ int SSL_add_file_cert_subjects_to_stack(STACK_OF(X509_NAME) *stack,
     X509_NAME *xn = NULL;
     int ret = 1;
     int (*oldcmp)(const X509_NAME * const *a, const X509_NAME * const *b);
-    
+
     oldcmp = sk_X509_NAME_set_cmp_func(stack, xname_cmp);
-    
+
     in = BIO_new(BIO_s_file_internal());
-    
+
     if (in == NULL) {
         SSLerr(SSL_F_SSL_ADD_FILE_CERT_SUBJECTS_TO_STACK, ERR_R_MALLOC_FAILURE);
         goto err;
     }
-    
+
     if (!BIO_read_filename(in, file))
         goto err;
-    
+
     for (;;) {
         if (PEM_read_bio_X509(in, &x, NULL, NULL) == NULL)
             break;
@@ -723,7 +723,7 @@ err:
         BIO_free(in);
     if (x != NULL)
         X509_free(x);
-    
+
     (void)sk_X509_NAME_set_cmp_func(stack, oldcmp);
 
     return ret;
@@ -751,8 +751,7 @@ int SSL_add_dir_cert_subjects_to_stack(STACK_OF(X509_NAME) *stack,
 
     /* Note that a side effect is that the CAs will be sorted by name */
 
-    while((filename = OPENSSL_DIR_read(&d, dir)))
-        {
+    while ((filename = OPENSSL_DIR_read(&d, dir))) {
         char buf[1024];
         int r;
 
@@ -766,7 +765,7 @@ int SSL_add_dir_cert_subjects_to_stack(STACK_OF(X509_NAME) *stack,
             goto err;
         if (!SSL_add_file_cert_subjects_to_stack(stack, buf))
             goto err;
-        }
+    }
 
     if (errno) {
         SYSerr(SYS_F_OPENDIR, errno);
@@ -783,4 +782,3 @@ err:
     CRYPTO_w_unlock(CRYPTO_LOCK_READDIR);
     return ret;
 }
-
