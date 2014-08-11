eb506f80aeb1e85170559b71c85b699017e1f907
Kurt Cancemi <kurt@x64Architecture.com>
2014-08-11 02:05:05 -0400

    Use SSL3_AL_FATAL not "2"
    Patch by: Matthieu Crapet

--- ssl/s3_pkt.c
+++ ssl/s3_pkt.c
@@ -1352,7 +1352,7 @@ int ssl3_send_alert(SSL *s, int level, int desc)
     if (desc < 0) 
         return -1;
     /* If a fatal one, remove from cache */
-    if ((level == 2) && (s->session != NULL))
+    if ((level == SSL3_AL_FATAL) && (s->session != NULL))
         SSL_CTX_remove_session(s->ctx, s->session);
 
     s->s3->alert_dispatch = 1;
