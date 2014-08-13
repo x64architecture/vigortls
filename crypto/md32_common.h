e1d58d334bca8b9730bd94d90e61033557ae4008
Kurt Cancemi <kurt@x64Architecture.com>
2014-08-12 20:32:34 -0400

    Replace ROTATE macros with an inline.
    The compiler can figure out for which architecture and optimize
    it accordingly.

--- crypto/md32_common.h
+++ crypto/md32_common.h
@@ -7,7 +7,7 @@
  * are met:
  *
  * 1. Redistributions of source code must retain the above copyright
- *    notice, this list of conditions and the following disclaimer. 
+ *    notice, this list of conditions and the following disclaimer.
  *
  * 2. Redistributions in binary form must reproduce the above copyright
  *    notice, this list of conditions and the following disclaimer in
@@ -109,6 +109,10 @@
  *                    <appro@fy.chalmers.se>
  */
 
+#include <stdint.h>
+
+#include <openssl/opensslconf.h>
+
 #if !defined(DATA_ORDER_IS_BIG_ENDIAN) && !defined(DATA_ORDER_IS_LITTLE_ENDIAN)
 #error "DATA_ORDER must be defined!"
 #endif
@@ -137,60 +141,10 @@
 #error "HASH_BLOCK_DATA_ORDER must be defined!"
 #endif
 
-/*
- * Engage compiler specific rotate intrinsic function if available.
- */
-#undef ROTATE
-#ifndef PEDANTIC
-#if defined(_MSC_VER) || defined(__ICC)
-#define ROTATE(a, n) _lrotl(a, n)
-#elif defined(__MWERKS__)
-#if defined(__POWERPC__)
-#define ROTATE(a, n) __rlwinm(a, n, 0, 31)
-#elif defined(__MC68K__)
-/* Motorola specific tweak. <appro@fy.chalmers.se> */
-#define ROTATE(a, n) (n < 24 ? __rol(a, n) : __ror(a, 32 - n))
-#else
-#define ROTATE(a, n) __rol(a, n)
-#endif
-#elif defined(__GNUC__) && __GNUC__ >= 2 && !defined(OPENSSL_NO_ASM) && !defined(OPENSSL_NO_INLINE_ASM)
-/*
-   * Some GNU C inline assembler templates. Note that these are
-   * rotates by *constant* number of bits! But that's exactly
-   * what we need here...
-   *                     <appro@fy.chalmers.se>
-   */
-#if defined(__i386) || defined(__i386__) || defined(__x86_64) || defined(__x86_64__)
-#define ROTATE(a, n) ({ register unsigned int ret;    \
-                asm (            \
-                "roll %1,%0"        \
-                : "=r"(ret)        \
-                : "I"(n), "0"((unsigned int)(a))    \
-                : "cc");        \
-               ret; \
-})
-#elif defined(_ARCH_PPC) || defined(_ARCH_PPC64) || defined(__powerpc) || defined(__ppc__) || defined(__powerpc64__)
-#define ROTATE(a, n) ({ register unsigned int ret;    \
-                asm (            \
-                "rlwinm %0,%1,%2,0,31"    \
-                : "=r"(ret)        \
-                : "r"(a), "I"(n));    \
-               ret; \
-})
-#elif defined(__s390x__)
-#define ROTATE(a, n) ({ register unsigned int ret;    \
-                asm ("rll %0,%1,%2"    \
-                : "=r"(ret)        \
-                : "r"(a), "I"(n));    \
-              ret; \
-})
-#endif
-#endif
-#endif /* PEDANTIC */
-
-#ifndef ROTATE
-#define ROTATE(a, n) (((a) << (n)) | (((a)&0xffffffff) >> (32 - (n))))
-#endif
+static inline uint32_t ROTATE(uint32_t a, unsigned int n)
+{
+    return ((a << n) | (a >> (32 - n)));
+}
 
 #if defined(DATA_ORDER_IS_BIG_ENDIAN)
 
@@ -203,12 +157,21 @@
      * this trick on x86* platforms only, because these CPUs can fetch
      * unaligned data without raising an exception.
      */
-#define HOST_c2l(c, l) ({ unsigned int r=*((const unsigned int *)(c));    \
-                   asm ("bswapl %0":"=r"(r):"0"(r));    \
-                   (c)+=4; (l)=r; })
-#define HOST_l2c(l, c) ({ unsigned int r=(l);            \
-                   asm ("bswapl %0":"=r"(r):"0"(r));    \
-                   *((unsigned int *)(c))=r; (c)+=4; r; })
+#define HOST_c2l(c, l)                                                                                                                                                                                                                                                                                                                             \
+    ({                                                                           \
+    unsigned int r = *((const unsigned int *)(c));                             \
+    asm("bswapl %0" : "=r"(r) : "0"(r));                                       \
+    (c) += 4;                                                                  \
+    (l) = r; \
+    })
+#define HOST_l2c(l, c)                                                                                                                                                                                                                                                                                                                                                                                                        \
+    ({                                                                           \
+    unsigned int r = (l);                                                      \
+    asm("bswapl %0" : "=r"(r) : "0"(r));                                       \
+    *((unsigned int *)(c)) = r;                                                \
+    (c) += 4;                                                                  \
+    r; \
+    })
 #endif
 #endif
 #endif
@@ -218,18 +181,18 @@
 #endif
 
 #ifndef HOST_c2l
-#define HOST_c2l(c, l) (l = (((unsigned long)(*((c)++))) << 24),  \
-                        l |= (((unsigned long)(*((c)++))) << 16), \
-                        l |= (((unsigned long)(*((c)++))) << 8),  \
-                        l |= (((unsigned long)(*((c)++)))),       \
-                        l)
+#define HOST_c2l(c, l)                         \
+    (l = (((unsigned long)(*((c)++))) << 24),  \
+     l |= (((unsigned long)(*((c)++))) << 16), \
+     l |= (((unsigned long)(*((c)++))) << 8),  \
+     l |= (((unsigned long)(*((c)++)))), l)
 #endif
 #ifndef HOST_l2c
-#define HOST_l2c(l, c) (*((c)++) = (unsigned char)(((l) >> 24) & 0xff), \
-                        *((c)++) = (unsigned char)(((l) >> 16) & 0xff), \
-                        *((c)++) = (unsigned char)(((l) >> 8) & 0xff),  \
-                        *((c)++) = (unsigned char)(((l)) & 0xff),       \
-                        l)
+#define HOST_l2c(l, c)                               \
+    (*((c)++) = (unsigned char)(((l) >> 24) & 0xff), \
+     *((c)++) = (unsigned char)(((l) >> 16) & 0xff), \
+     *((c)++) = (unsigned char)(((l) >> 8) & 0xff),  \
+     *((c)++) = (unsigned char)(((l)) & 0xff), l)
 #endif
 
 #elif defined(DATA_ORDER_IS_LITTLE_ENDIAN)
@@ -237,12 +200,18 @@
 #ifndef PEDANTIC
 #if defined(__GNUC__) && __GNUC__ >= 2 && !defined(OPENSSL_NO_ASM) && !defined(OPENSSL_NO_INLINE_ASM)
 #if defined(__s390x__)
-#define HOST_c2l(c, l) ({ asm ("lrv    %0,%1"            \
-                   :"=d"(l) :"m"(*(const unsigned int *)(c)));\
-                   (c)+=4; (l); })
-#define HOST_l2c(l, c) ({ asm ("strv    %1,%0"            \
-                   :"=m"(*(unsigned int *)(c)) :"d"(l));\
-                   (c)+=4; (l); })
+#define HOST_c2l(c, l)                                                                                                                                                                                                                                        \
+    ({                                                                           \
+    asm("lrv    %0,%1" : "=d"(l) : "m"(*(const unsigned int *)(c)));           \
+    (c) += 4;                                                                  \
+    (l); \
+    })
+#define HOST_l2c(l, c)                                                                                                                                                                                                                                        \
+    ({                                                                           \
+    asm("strv    %1,%0" : "=m"(*(unsigned int *)(c)) : "d"(l));                \
+    (c) += 4;                                                                  \
+    (l); \
+    })
 #endif
 #endif
 #endif
@@ -252,18 +221,17 @@
 #endif
 
 #ifndef HOST_c2l
-#define HOST_c2l(c, l) (l = (((unsigned long)(*((c)++)))),        \
-                        l |= (((unsigned long)(*((c)++))) << 8),  \
-                        l |= (((unsigned long)(*((c)++))) << 16), \
-                        l |= (((unsigned long)(*((c)++))) << 24), \
-                        l)
+#define HOST_c2l(c, l)                                                           \
+    (l = (((unsigned long)(*((c)++)))), l |= (((unsigned long)(*((c)++))) << 8), \
+     l |= (((unsigned long)(*((c)++))) << 16),                                   \
+     l |= (((unsigned long)(*((c)++))) << 24), l)
 #endif
 #ifndef HOST_l2c
-#define HOST_l2c(l, c) (*((c)++) = (unsigned char)(((l)) & 0xff),       \
-                        *((c)++) = (unsigned char)(((l) >> 8) & 0xff),  \
-                        *((c)++) = (unsigned char)(((l) >> 16) & 0xff), \
-                        *((c)++) = (unsigned char)(((l) >> 24) & 0xff), \
-                        l)
+#define HOST_l2c(l, c)                               \
+    (*((c)++) = (unsigned char)(((l)) & 0xff),       \
+     *((c)++) = (unsigned char)(((l) >> 8) & 0xff),  \
+     *((c)++) = (unsigned char)(((l) >> 16) & 0xff), \
+     *((c)++) = (unsigned char)(((l) >> 24) & 0xff), l)
 #endif
 
 #endif
@@ -284,7 +252,7 @@ int HASH_UPDATE(HASH_CTX *c, const void *data_, size_t len)
 
     l = (c->Nl + (((HASH_LONG)len) << 3)) & 0xffffffffUL;
     /* 95-05-24 eay Fixed a bug with the overflow handling, thanks to
-     * Wei Dai <weidai@eskimo.com> for pointing it out. */
+   * Wei Dai <weidai@eskimo.com> for pointing it out. */
     if (l < c->Nl) /* overflow */
         c->Nh++;
     c->Nh += (HASH_LONG)(len >> 29); /* might cause compiler warning on 16-bit */
@@ -383,7 +351,7 @@ int HASH_FINAL(unsigned char *md, HASH_CTX *c)
  * *either* case. Now declaring 'em long excuses the compiler
  * from keeping 32 MSBs zeroed resulting in 13% performance
  * improvement under SPARC Solaris7/64 and 5% under AlphaLinux.
- * Well, to be honest it should say that this *prevents* 
+ * Well, to be honest it should say that this *prevents*
  * performance degradation.
  *                <appro@fy.chalmers.se>
  */
