13dc4c8516db993cf272b486783f7b1a360633e8
Kurt Cancemi <kurt@x64architecture.com>
2015-01-11 15:19:37 -0500

    Preserve digests for SNI.
    SSL_set_SSL_CTX is normally called for SNI after ClientHello has
    received and the digest to use for each certificate has been decided.
    The original ssl->cert contains the negotiated digests and is now
    copied to the new ssl->cert.
    
    Patch by: Dr. Stephen Henson <steve@openssl.org>

--- ssl/ssl_lib.c
+++ ssl/ssl_lib.c
@@ -2553,16 +2553,13 @@ SSL_CTX *SSL_set_SSL_CTX(SSL *ssl, SSL_CTX *ctx)
     if (ctx == NULL)
         ctx = ssl->initial_ctx;
     ssl->cert = ssl_cert_dup(ctx->cert);
-    if (ocert) {
-        /* Preserve any already negotiated parameters */
-        if (ssl->server)
-        {
-            ssl->cert->peer_sigalgs = ocert->peer_sigalgs;
-            ssl->cert->peer_sigalgslen = ocert->peer_sigalgslen;
-            ocert->peer_sigalgs = NULL;
-            ssl->cert->ciphers_raw = ocert->ciphers_raw;
-            ssl->cert->ciphers_rawlen = ocert->ciphers_rawlen;
-            ocert->ciphers_raw = NULL;
+    if (ocert != NULL) {
+        int i;
+        /* Copy negotiated digests from original */
+        for (i = 0; i < SSL_PKEY_NUM; i++) {
+            CERT_PKEY *cpk = ocert->pkeys + i;
+            CERT_PKEY *rpk = ssl->cert->pkeys + i;
+            rpk->digest = cpk->digest;
         }
         ssl_cert_free(ocert);
     }
