573eefd128c709a49cc5e6a729f9d0325ffb08e1
Kurt Cancemi <kurt@x64architecture.com>
2015-01-26 11:08:00 -0500

    Rewrite windows rand code

--- crypto/rand/windows.c
+++ crypto/rand/windows.c
@@ -1,4 +1,5 @@
-/* Copyright (c) 2014, Google Inc.
+/*
+ * Copyright (c) 2015, Kurt Cancemi (kurt@x64architecture.com)
  *
  * Permission to use, copy, modify, and/or distribute this software for any
  * purpose with or without fee is hereby granted, provided that the above
@@ -6,11 +7,12 @@
  *
  * THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
  * WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
- * MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY
- * SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
- * WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN ACTION
- * OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF OR IN
- * CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE. */
+ * MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR
+ * ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
+ * WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN
+ * ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF
+ * OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+ */
 
 #include <openssl/rand.h>
 
@@ -18,11 +20,11 @@
 
 #include <limits.h>
 #include <stdlib.h>
-#include <Windows.h>
+#include <windows.h>
 
-/* #define needed to link in RtlGenRandom(), a.k.a. SystemFunction036.  See the
- * "Community Additions" comment on MSDN here:
- * http://msdn.microsoft.com/en-us/library/windows/desktop/aa387694.aspx */
+#define FAILURE 0
+#define SUCCESS 1
+
 #define SystemFunction036 NTAPI SystemFunction036
 #include <NTSecAPI.h>
 #undef SystemFunction036
@@ -31,21 +33,21 @@ void RAND_cleanup(void)
 {
 }
 
-int RAND_bytes(uint8_t *out, size_t requested)
+int RAND_bytes(uint8_t *buf, size_t requested)
 {
     while (requested > 0) {
-        ULONG output_bytes_this_pass = ULONG_MAX;
-        if (requested < output_bytes_this_pass) {
-            output_bytes_this_pass = requested;
+        unsigned long output_bytes;
+        if (requested < ULONG_MAX) {
+            output_bytes = requested;
         }
-        if (RtlGenRandom(out, output_bytes_this_pass) == FALSE) {
+        if (RtlGenRandom(buf, output_bytes) == FALSE) {
             abort();
-            return 0;
+            return FAILURE;
         }
-        requested -= output_bytes_this_pass;
-        out += output_bytes_this_pass;
+        requested -= output_bytes;
+        buf += output_bytes;
     }
-    return 1;
+    return SUCCESS;
 }
 
 #endif /* _WIN32 */
