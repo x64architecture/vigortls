d2a332fb23e786ee2c6ded3822e6b759c05331a4
Kurt Cancemi <kurt@x64architecture.com>
2015-03-19 15:47:02 -0400

    PKCS#7: avoid NULL pointer dereferences with missing content
    In PKCS#7, the ASN.1 content component is optional.
    This typically applies to inner content (detached signatures),
    however we must also handle unexpected missing outer content
    correctly.
    
    This patch only addresses functions reachable from parsing,
    decryption and verification, and functions otherwise associated
    with reading potentially untrusted data.
    
    Correcting all low-level API calls requires further work.
    
    CVE-2015-0289
    
    Thanks to Michal Zalewski (Google) for reporting this issue.
    From OpenSSL

--- crypto/pkcs7/pk7_lib.c
+++ crypto/pkcs7/pk7_lib.c
@@ -71,6 +71,7 @@ long PKCS7_ctrl(PKCS7 *p7, int cmd, long larg, char *parg)
     nid = OBJ_obj2nid(p7->type);
 
     switch (cmd) {
+        /* NOTE(emilia): does not support detached digested data. */
         case PKCS7_OP_SET_DETACHED_SIGNATURE:
             if (nid == NID_pkcs7_signed) {
                 ret = p7->detached = (int)larg;
@@ -441,8 +442,10 @@ int PKCS7_set_digest(PKCS7 *p7, const EVP_MD *md)
     return 1;
 }
 
-STACK_OF(PKCS7_SIGNER_INFO) * PKCS7_get_signer_info(PKCS7 *p7)
+STACK_OF(PKCS7_SIGNER_INFO) *PKCS7_get_signer_info(PKCS7 *p7)
 {
+    if (p7 == NULL || p7->d.ptr == NULL)
+        return NULL;
     if (PKCS7_type_is_signed(p7)) {
         return (p7->d.sign->signer_info);
     } else if (PKCS7_type_is_signedAndEnveloped(p7)) {
