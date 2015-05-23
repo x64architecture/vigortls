545c724d5b62449b29f98d0d3b3790402eac2807
Kurt Cancemi <kurt@x64architecture.com>
2015-05-23 13:28:30 -0400

    Lost alert in DTLS
    If a client receives a bad hello request in DTLS then the alert is not
    sent correctly.
    
    From Lubomír Sedlář

--- ssl/d1_pkt.c
+++ ssl/d1_pkt.c
@@ -853,7 +853,7 @@ start:
             || (s->d1->handshake_fragment[3] != 0)) {
             al = SSL_AD_DECODE_ERROR;
             SSLerr(SSL_F_DTLS1_READ_BYTES, SSL_R_BAD_HELLO_REQUEST);
-            goto err;
+            goto f_err;
         }
 
         /* no need to check sequence number on HELLO REQUEST messages */
