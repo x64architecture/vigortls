d54dfc544922c6214e87e8531075f7f496adccc4
Kurt Cancemi <kurt@x64architecture.com>
2015-12-26 23:26:38 -0500

    Don't check RSA_FLAG_SIGN_VER
    From OpenSSL

--- crypto/rsa/rsa_sign.c
+++ crypto/rsa/rsa_sign.c
@@ -79,7 +79,7 @@ int RSA_sign(int type, const uint8_t *m, unsigned int m_len,
     const uint8_t *s = NULL;
     X509_ALGOR algor;
     ASN1_OCTET_STRING digest;
-    if ((rsa->flags & RSA_FLAG_SIGN_VER) && rsa->meth->rsa_sign) {
+    if (rsa->meth->rsa_sign) {
         return rsa->meth->rsa_sign(type, m, m_len,
                                    sigret, siglen, rsa);
     }
@@ -241,7 +241,7 @@ int RSA_verify(int dtype, const uint8_t *m, unsigned int m_len,
                RSA *rsa)
 {
 
-    if ((rsa->flags & RSA_FLAG_SIGN_VER) && rsa->meth->rsa_verify) {
+    if (rsa->meth->rsa_verify) {
         return rsa->meth->rsa_verify(dtype, m, m_len,
                                      sigbuf, siglen, rsa);
     }
