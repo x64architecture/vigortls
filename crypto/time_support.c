68f14fc3f66494bcc8dbf6a9dd54fb6bfc0adf30
Kurt Cancemi <kurt@x64architecture.com>
2016-01-18 15:15:33 -0500

    Improve style in time_support.c

--- crypto/time_support.c
+++ crypto/time_support.c
@@ -1,68 +1,25 @@
-/* Written by Richard Levitte (richard@levitte.org) for the OpenSSL
- * project 2001.
- */
-/* Written by Dr Stephen N Henson (steve@openssl.org) for the OpenSSL
- * project 2008.
- */
-/* ====================================================================
- * Copyright (c) 2001 The OpenSSL Project.  All rights reserved.
+/*
+ * Copyright (C) 2016, Kurt Cancemi
  *
- * Redistribution and use in source and binary forms, with or without
- * modification, are permitted provided that the following conditions
- * are met:
+ * Permission to use, copy, modify, and/or distribute this software for any
+ * purpose with or without fee is hereby granted, provided that the above
+ * copyright notice and this permission notice appear in all copies.
  *
- * 1. Redistributions of source code must retain the above copyright
- *    notice, this list of conditions and the following disclaimer.
- *
- * 2. Redistributions in binary form must reproduce the above copyright
- *    notice, this list of conditions and the following disclaimer in
- *    the documentation and/or other materials provided with the
- *    distribution.
- *
- * 3. All advertising materials mentioning features or use of this
- *    software must display the following acknowledgment:
- *    "This product includes software developed by the OpenSSL Project
- *    for use in the OpenSSL Toolkit. (http://www.OpenSSL.org/)"
- *
- * 4. The names "OpenSSL Toolkit" and "OpenSSL Project" must not be used to
- *    endorse or promote products derived from this software without
- *    prior written permission. For written permission, please contact
- *    licensing@OpenSSL.org.
- *
- * 5. Products derived from this software may not be called "OpenSSL"
- *    nor may "OpenSSL" appear in their names without prior written
- *    permission of the OpenSSL Project.
- *
- * 6. Redistributions of any form whatsoever must retain the following
- *    acknowledgment:
- *    "This product includes software developed by the OpenSSL Project
- *    for use in the OpenSSL Toolkit (http://www.OpenSSL.org/)"
- *
- * THIS SOFTWARE IS PROVIDED BY THE OpenSSL PROJECT ``AS IS'' AND ANY
- * EXPRESSED OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
- * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
- * PURPOSE ARE DISCLAIMED.  IN NO EVENT SHALL THE OpenSSL PROJECT OR
- * ITS CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
- * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT
- * NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
- * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
- * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT,
- * STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
- * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED
- * OF THE POSSIBILITY OF SUCH DAMAGE.
- * ====================================================================
- *
- * This product includes cryptographic software written by Eric Young
- * (eay@cryptsoft.com).  This product includes software written by Tim
- * Hudson (tjh@cryptsoft.com).
- *
+ * THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
+ * WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
+ * MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR
+ * ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
+ * WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN
+ * ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF
+ * OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
  */
 
 #include <string.h>
 
 #include "time_support.h"
 
-/* Take a tm structure and add an offset to it. This avoids any OS issues
+/*
+ * Take a tm structure and add an offset to it. This avoids any OS issues
  * with restricted date types and overflows which cause the year 2038
  * problem.
  */
@@ -77,7 +34,7 @@ int gmtime_r_adj(struct tm *tm, int off_day, long offset_sec)
     int offset_hms, offset_day;
     long time_jd;
     int time_year, time_month, time_day;
-    /* split offset into days and day seconds */
+    /* Split offset into days and day seconds */
     offset_day = offset_sec / SECS_PER_DAY;
     /* Avoid sign issues with % operator */
     offset_hms = offset_sec - (offset_day * SECS_PER_DAY);
@@ -93,8 +50,7 @@ int gmtime_r_adj(struct tm *tm, int off_day, long offset_sec)
         offset_hms += SECS_PER_DAY;
     }
 
-    /* Convert date of time structure into a Julian day number.
-     */
+    /* Convert date of time structure into a Julian day number. */
 
     time_year = tm->tm_year + 1900;
     time_month = tm->tm_mon + 1;
@@ -128,26 +84,34 @@ int gmtime_r_adj(struct tm *tm, int off_day, long offset_sec)
     return 1;
 }
 
-/* Convert date to and from julian day
- * Uses Fliegel & Van Flandern algorithm
+/*
+ * Convert date to julian
+ * Uses Fliegel & Van Flandern Algorithm
  */
 static long date_to_julian(int y, int m, int d)
 {
-    return (1461 * (y + 4800 + (m - 14) / 12)) / 4 + (367 * (m - 2 - 12 * ((m - 14) / 12))) / 12 - (3 * ((y + 4900 + (m - 14) / 12) / 100)) / 4 + d - 32075;
+    int m1 = (m - 14) / 12;
+    int y1 = y + 4800;
+
+    return (1461 * (y1 + m1) / 4 + 367 * (m - 2 - 12 * m1) / 12 - (3 * ((y1 + m1 + 100) / 100)) / 4 + d - 32075);
 }
 
+/*
+ * Convert julian to date
+ * Uses Fliegel & Van Flandern Algorithm
+ */
 static void julian_to_date(long jd, int *y, int *m, int *d)
 {
-    long L = jd + 68569;
-    long n = (4 * L) / 146097;
-    long i, j;
+    long l, n, i, j;
 
-    L = L - (146097 * n + 3) / 4;
-    i = (4000 * (L + 1)) / 1461001;
-    L = L - (1461 * i) / 4 + 31;
-    j = (80 * L) / 2447;
-    *d = L - (2447 * j) / 80;
-    L = j / 11;
-    *m = j + 2 - (12 * L);
-    *y = 100 * (n - 49) + i + L;
+    l = jd + 68569;
+    n = (4 * l) / 146097;
+    l = l - (146097 * n + 3) / 4;
+    i = (4000 * (l + 1)) / 1461001;
+    l = l - (1461 * i) / 4 + 31;
+    j = (80 * l) / 2447;
+    *d = l - (2447 * j) / 80;
+    l = j / 11;
+    *m = j + 2 - (12 * l);
+    *y = 100 * (n - 49) + i + l;
 }
