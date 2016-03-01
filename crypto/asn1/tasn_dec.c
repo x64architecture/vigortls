e9ef3f21ecdacd2a3cd0caf7ecaca2af47eadde9
Kurt Cancemi <kurt@x64Architecture.com>
2016-03-01 15:10:01 -0500

    Fix possible memory leak on BUF_MEM_grow_clean failure
    From OpenSSL

--- crypto/asn1/tasn_dec.c
+++ crypto/asn1/tasn_dec.c
@@ -641,8 +641,7 @@ static int asn1_d2i_ex_primitive(ASN1_VALUE **pval,
     long plen;
     char cst, inf, free_cont = 0;
     const uint8_t *p;
-    BUF_MEM buf;
-    buf.data = NULL;
+    BUF_MEM buf = { 0, NULL, 0 }
     const uint8_t *cont = NULL;
     long len;
     if (!pval) {
@@ -719,39 +718,34 @@ static int asn1_d2i_ex_primitive(ASN1_VALUE **pval,
         } else {
             len = p - cont + plen;
             p += plen;
-            buf.data = NULL;
         }
     } else if (cst) {
-        if (utype == V_ASN1_NULL || utype == V_ASN1_BOOLEAN
-            || utype == V_ASN1_OBJECT || utype == V_ASN1_INTEGER
-            || utype == V_ASN1_ENUMERATED)
-        {
+        if (utype == V_ASN1_NULL || utype == V_ASN1_BOOLEAN ||
+            utype == V_ASN1_OBJECT || utype == V_ASN1_INTEGER ||
+            utype == V_ASN1_ENUMERATED) {
             ASN1err(ASN1_F_ASN1_D2I_EX_PRIMITIVE, ASN1_R_TYPE_NOT_PRIMITIVE);
             return 0;
         }
-        buf.length = 0;
-        buf.max = 0;
-        buf.data = NULL;
+        
+        /* Free any returned 'buf' content */
+        free_cont = 1;
         /* Should really check the internal tags are correct but
          * some things may get this wrong. The relevant specs
          * say that constructed string types should be OCTET STRINGs
          * internally irrespective of the type. So instead just check
          * for UNIVERSAL class and ignore the tag.
          */
-        if (!asn1_collect(&buf, &p, plen, inf, -1, V_ASN1_UNIVERSAL, 0)) {
-            free_cont = 1;
+        if (!asn1_collect(&buf, &p, plen, inf, -1, V_ASN1_UNIVERSAL, 0))
             goto err;
-        }
         len = buf.length;
         /* Append a final null to string */
         if (!BUF_MEM_grow_clean(&buf, len + 1)) {
             ASN1err(ASN1_F_ASN1_D2I_EX_PRIMITIVE,
                     ERR_R_MALLOC_FAILURE);
-            return 0;
+            goto err;
         }
         buf.data[len] = 0;
         cont = (const uint8_t *)buf.data;
-        free_cont = 1;
     } else {
         cont = p;
         len = plen;
@@ -759,6 +753,7 @@ static int asn1_d2i_ex_primitive(ASN1_VALUE **pval,
     }
 
     /* We now have content length and type: translate into a structure */
+    /* asn1_ex_c2i may reuse allocated buffer, and so sets free_cont to 0 */
     if (!asn1_ex_c2i(pval, cont, len, utype, &free_cont, it))
         goto err;
 
