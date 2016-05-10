f1dd708fc11f07abdaa6acbbb29be8e2b98222ee
Kurt Cancemi <kurt@x64architecture.com>
2016-05-09 23:44:05 -0400

    malloc+memset -> calloc

--- crypto/asn1/tasn_new.c
+++ crypto/asn1/tasn_new.c
@@ -127,10 +127,9 @@ int ASN1_item_ex_new(ASN1_VALUE **pval, const ASN1_ITEM *it)
                 if (i == 2)
                     return 1;
             }
-            *pval = malloc(it->size);
+            *pval = calloc(1, it->size);
             if (*pval == NULL)
                 goto memerr;
-            memset(*pval, 0, it->size);
             asn1_set_choice_selector(pval, -1, it);
             if (asn1_cb && !asn1_cb(ASN1_OP_NEW_POST, pval, it, NULL))
                 goto auxerr;
@@ -145,10 +144,9 @@ int ASN1_item_ex_new(ASN1_VALUE **pval, const ASN1_ITEM *it)
                 if (i == 2)
                     return 1;
             }
-            *pval = malloc(it->size);
+            *pval = calloc(1, it->size);
             if (*pval == NULL)
                 goto memerr;
-            memset(*pval, 0, it->size);
             asn1_do_lock(pval, 0, it);
             asn1_enc_init(pval, it);
             for (i = 0, tt = it->templates; i < it->tcount; tt++, i++) {
