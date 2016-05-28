ee8bd281efab36c671df4611cd390f626409710f
Kurt Cancemi <kurt@x64architecture.com>
2016-05-28 04:54:44 -0400

    Print out issuer and subject unique identifier fields in certificates
    From OpenSSL

--- include/openssl/x509.h
+++ include/openssl/x509.h
@@ -256,6 +256,7 @@ DECLARE_STACK_OF(X509_TRUST)
 #define X509_FLAG_NO_SIGDUMP (1L << 9)
 #define X509_FLAG_NO_AUX (1L << 10)
 #define X509_FLAG_NO_ATTRIBUTES (1L << 11)
+#define X509_FLAG_NO_IDS (1L << 12)
 
 /* Flags specific to X509_NAME_print_ex() */
 
