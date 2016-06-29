d58e1ad0389e8604014bd215f55394e6c7e2448a
Kurt Cancemi <kurt@x64architecture.com>
2016-06-29 03:24:54 -0400

    Allow multiple IDN xn-- indicators
    Update the X509v3 name parsing to allow multiple xn-- international
    domain name indicators in a name.  Previously, only allowed one at
    the beginning of a name, which was wrong.
    
    Based on OpenSSL

--- crypto/x509v3/v3_utl.c
+++ crypto/x509v3/v3_utl.c
@@ -736,38 +736,23 @@ static const uint8_t *valid_star(const uint8_t *p, size_t len,
             star = &p[i];
             state &= ~LABEL_START;
         }
-        else if ((state & LABEL_START) != 0) {
-            /*
-             * At the start of a label, skip any "xn--" and
-             * remain in the LABEL_START state, but set the
-             * IDNA label state
-             */
-            if ((state & LABEL_IDNA) == 0 && len - i >= 4 &&
-                strncasecmp((char *)&p[i], "xn--", 4) == 0) {
-                i += 3;
-                state |= LABEL_IDNA;
-                continue;
-            }
-            /* Labels must start with a letter or digit */
-            state &= ~LABEL_START;
-            if (('a' <= p[i] && p[i] <= 'z') || ('A' <= p[i] && p[i] <= 'Z') ||
-                ('0' <= p[i] && p[i] <= '9'))
-                continue;
-            return NULL;
-        }
         else if (('a' <= p[i] && p[i] <= 'z') || ('A' <= p[i] && p[i] <= 'Z') ||
                  ('0' <= p[i] && p[i] <= '9')) {
-            state &= LABEL_IDNA;
-            continue;
+            if ((state & LABEL_START) != 0 && len - i >= 4 &&
+                strncasecmp((char *)&p[i], "xn--", 4) == 0)
+            {
+                state |= LABEL_IDNA;
+            }
+            state &= ~(LABEL_HYPHEN|LABEL_START);
         }
         else if (p[i] == '.') {
-            if (state & (LABEL_HYPHEN | LABEL_START))
+            if ((state & (LABEL_HYPHEN | LABEL_START)) != 0)
                 return NULL;
             state = LABEL_START;
             ++dots;
         }
         else if (p[i] == '-') {
-            if (state & LABEL_HYPHEN)
+            if ((state & LABEL_HYPHEN) != 0)
                 return NULL;
             state |= LABEL_HYPHEN;
         }
