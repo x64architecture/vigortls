02a27f7b9d8c7aaa1296578529de9d5af054c467
Kurt Cancemi <kurt@x64architecture.com>
2016-07-02 13:05:40 -0400

    Return correct NID for undefined object
    From OpenSSL

--- crypto/objects/obj_dat.c
+++ crypto/objects/obj_dat.c
@@ -343,21 +343,24 @@ int OBJ_obj2nid(const ASN1_OBJECT *a)
     ADDED_OBJ ad, *adp;
 
     if (a == NULL)
-        return (NID_undef);
+        return NID_undef;
     if (a->nid != 0)
-        return (a->nid);
+        return a->nid;
+
+    if (a->length == 0)
+        return NID_undef;
 
     if (added != NULL) {
         ad.type = ADDED_DATA;
         ad.obj = (ASN1_OBJECT *)a; /* XXX: ugly but harmless */
         adp = lh_ADDED_OBJ_retrieve(added, &ad);
         if (adp != NULL)
-            return (adp->obj->nid);
+            return adp->obj->nid;
     }
     op = OBJ_bsearch_obj(&a, obj_objs, NUM_OBJ);
     if (op == NULL)
-        return (NID_undef);
-    return (nid_objs[*op].nid);
+        return NID_undef;
+    return nid_objs[*op].nid;
 }
 
 /* Convert an object name into an ASN1_OBJECT
