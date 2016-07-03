x64architecture/vigortls

commit ea92c6a24d3446567f68c053b3abd1fc34a0fdd0

Fix SSL_set_session_ticket_ext when used with SSLv23_method
The function SSL_set_session_ticket_ext can be used to set custom session
ticket data passed in the initial ClientHello. This can be particularly
useful for EAP-FAST. However, when using SSLv23_method, the session does
not get created until the ServerHello has been received. The extension code
will only add the SessionTicket data to the ClientHello if a session already
exists. Therefore SSL_set_session_ticket_ext has no impact when used in
conjunction with SSLv23_method. The solution is to simply create the session
during creation of the ClientHello instead of waiting for the ServerHello.

ssl/s23_clnt.c
@@ -180,6 +180,14 @@ static int ssl23_client_hello(SSL *s)
 
     buf = (uint8_t *)s->init_buf->data;
     if (s->state == SSL23_ST_CW_CLNT_HELLO_A) {
+        /*
+         * Since we're sending s23 client hello, we're not reusing a session, as
+         * we'd be using the method from the saved session instead
+         */
+        if (!ssl_get_new_session(s, 0)) {
+            return -1;
+        }
+
         p = s->s3->client_random;
         if (ssl_fill_hello_random(s, p, SSL3_RANDOM_SIZE) <= 0) {
             SSLerr(SSL_F_SSL23_CLIENT_HELLO, ERR_R_INTERNAL_ERROR);
@@ -409,13 +417,6 @@ static int ssl23_get_server_hello(SSL *s)
         goto err;
     }
     s->init_num = 0;
-
-    /*
-     * Since, if we are sending a ssl23 client hello, we are not
-     * reusing a session-id
-     */
-    if (!ssl_get_new_session(s, 0))
-        goto err;
 
     return (SSL_connect(s));
 err:

commit 23c4d4a70fac88ee0872557ec4ba5fb08bdf658a

Fix session resumption
Commit ea92c6a introduced a problem with session resumption. The
version for the session is fixed when the session is created. By moving
the creation of the session earlier in the process the version is fixed
*before* version negotiation has completed when processing the ServerHello
on the client side. This fix updates the session version after version neg
has completed.

From OpenSSL

ssl/s23_clnt.c
@@ -362,6 +362,8 @@ static int ssl23_get_server_hello(SSL *s)
             goto err;
         }
 
+        s->session->ssl_version = s->version;
+
         /* ensure that TLS_MAX_VERSION is up-to-date */
         OPENSSL_assert(s->version <= TLS_MAX_VERSION);
 