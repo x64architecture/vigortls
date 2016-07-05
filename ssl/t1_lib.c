cd0dd17be413efc35e7ba77481abe92503ef1bb2
Kurt Cancemi <kurt@x64architecture.com>
2016-07-05 14:57:34 -0400

    tls1_lookup_sigalg(): Add a return value check
    If the call to OBJ_find_sigid_by_algs fails to find the relevant NID then
    we should set the NID to NID_undef.
    
    Based on OpenSSL

--- ssl/t1_lib.c
+++ ssl/t1_lib.c
@@ -2687,7 +2687,7 @@ static int tls12_get_pkey_idx(uint8_t sig_alg)
 static void tls1_lookup_sigalg(int *phash_nid, int *psign_nid,
                                int *psignhash_nid, const uint8_t *data)
 {
-    int sign_nid, hash_nid;
+    int sign_nid = NID_undef, hash_nid = NID_undef;
     if (!phash_nid && !psign_nid && !psignhash_nid)
         return;
     if (phash_nid  != NULL || psignhash_nid != NULL) {
@@ -2703,9 +2703,8 @@ static void tls1_lookup_sigalg(int *phash_nid, int *psign_nid,
             *psign_nid = sign_nid;
     }
     if (psignhash_nid != NULL) {
-        if (sign_nid && hash_nid)
-            OBJ_find_sigid_by_algs(psignhash_nid, hash_nid, sign_nid);
-        else
+        if (sign_nid == NID_undef || hash_nid == NID_undef ||
+            OBJ_find_sigid_by_algs(psignhash_nid, hash_nid, sign_nid) <= 0)
             *psignhash_nid = NID_undef;
     }
 }
